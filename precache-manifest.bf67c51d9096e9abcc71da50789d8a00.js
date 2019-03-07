3d5b58b05dec6881fe837df36c04c4336f4bb699
jomorro <morrowjoe0@gmail.com>
2019-03-07 09:45:52 -0500

    Updates

---
+++ precache-manifest.bf67c51d9096e9abcc71da50789d8a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56578612cadf6faa79ed",
+    "url": "/todos-part-2/static/js/runtime~main.56578612.js"
+  },
+  {
+    "revision": "6704995ce53b21a3c315",
+    "url": "/todos-part-2/static/js/main.6704995c.chunk.js"
+  },
+  {
+    "revision": "b3c0318b0613ee66eaf4",
+    "url": "/todos-part-2/static/js/1.b3c0318b.chunk.js"
+  },
+  {
+    "revision": "6704995ce53b21a3c315",
+    "url": "/todos-part-2/static/css/main.573e1bf4.chunk.css"
+  },
+  {
+    "revision": "207f24ef7273b66de9ec3d94ed66475b",
+    "url": "/todos-part-2/index.html"
+  }
+];
